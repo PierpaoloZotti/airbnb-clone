88efc12ab156d4a49421ed2394c6b4324be7abf1
rweventos <rweventos.rj@gmail.com>
2023-05-30 04:19:15 -0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import RegisterModal from "./components/modals/RegisterModal";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import ToasterProvider from "./providers/ToasterProvider";
+
+const nunito = Nunito({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "zetabnb",
+  description: "AirBnB Clone",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className={nunito.className}>
+        <ToasterProvider />
+        <RegisterModal />
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
