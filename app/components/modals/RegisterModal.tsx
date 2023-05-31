6259e0f54665de8b79b04f6ba685e86a2a0a9f18
rweventos <rweventos.rj@gmail.com>
2023-05-31 10:32:49 -0300

    Ultimated login authentication with google and github

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -12,6 +12,7 @@ import Heading from "../Heading";
 import Input from "../inputs/Input";
 import { toast } from "react-hot-toast";
 import Button from "../Button";
+import { signIn } from "next-auth/react";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -86,20 +87,23 @@ const RegisterModal = () => {
         outline
         label='Continue with Google'
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => signIn("google")}
       />
       <Button
         outline
         label='Continue with Github'
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn("github")}
       />
       <div className='text-neutral-500 text-center mt-4 font-light'>
         <div className='flex flex-row items-center gap-2 justify-center'>
           <div>Already have an account?</div>
-          <div 
-          onClick={registerModal.onClose}
-          className='text-neutral-800 cursor-pointer hover:underline'>Log in</div>
+          <div
+            onClick={registerModal.onClose}
+            className='text-neutral-800 cursor-pointer hover:underline'
+          >
+            Log in
+          </div>
         </div>
       </div>
     </div>
